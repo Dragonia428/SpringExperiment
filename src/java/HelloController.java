825605d60dd640057d174d5c2528696e4abdd990
John Settineri <settineri.john@gmail.com>
2018-10-25 23:08:12 -0400

    Adding project to learn Spring Framework

---
+++ src/java/HelloController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.validation.BindException;
+
+import org.springframework.web.servlet.mvc.SimpleFormController;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ *
+ * @author John G Settineri
+ */
+public class HelloController extends SimpleFormController {
+    
+    public HelloController() {
+        //Initialize controller properties here or 
+        //in the Web Application Context
+
+        setCommandClass(Name.class);
+        setCommandName("name");
+        setSuccessView("helloView");
+        setFormView("nameView");
+    }
+    
+    
+    @Override
+    protected ModelAndView onSubmit(
+            HttpServletRequest request, 
+            HttpServletResponse response, 
+            Object command, 
+            BindException errors) throws Exception {
+        Name name = (Name) command;
+        ModelAndView mv = new ModelAndView(getSuccessView());
+        mv.addObject("helloMessage", helloService.sayHello(name.getValue()));
+        //Do something...
+        return mv;
+    }
+     
+}
